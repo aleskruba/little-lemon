8ccc3a6f6723e57cf2f92770fd01d2bd34f240cb
Ales <aleskruba@seznam.cz>
2024-11-20 17:13:27 +0100

    add meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div >
+        <div >
+
+        </div>
+
+        <div>
+            <h1>Navigation</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+        </div>
+        <div>
+         <h1>Contact</h1>
+            <ul>
+                <li>Phone Number</li>
+                <li>Email</li>
+                <li>Address</li>            
+            </ul>
+        </div>
+        <div>   
+            <h1>Social Media
+            </h1>
+            <ul>
+                <li>Instagram</li>
+                <li>LinkedIn</li>
+                <li>Pinterest</li>  
+            </ul>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Nav
