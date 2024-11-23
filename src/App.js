14f8f34e354cd0667c349d84b8ca051156852142
Ales <aleskruba@seznam.cz>
2024-11-23 15:23:20 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import About from './components/About';
 import Login from './components/Login';
 import Menu from './components/Menu';
 import Order from './components/Order';
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Confirmation from './components/Confirmation';
 
-
 function App() {
   return (
-<> 
-<div className='header-div' data-testid="header-div">
-      <Header/>
-      <Nav/>
-    </div>
- <Routes>
-        <Route path="/" element={<Home />}>  </Route>
-        <Route path="/menu" element={<Menu />}> </Route>
-        <Route path="/order" element={<Order />}> </Route>
-        <Route path="/about" element={<About />}> </Route>
-        <Route path="/login" element={<Login />}> </Route>
-        <Route path="/confirmation" element={<Confirmation />} />
-    </Routes>
-   </>
-
+    <>
+      <header className='header-div' data-testid="header-div">
+        <Header />
+      </header>
+      <nav>
+        <Nav />
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
